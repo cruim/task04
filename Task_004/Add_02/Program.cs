677664ee6ec49224bf6f73c3b134e4cdbaaee0fa
Daniil Arashkevich <daniil.arashkevich@gmail.com>
2016-03-17 23:38:56 +0300

    second commit

---
+++ Task_004/Add_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Add_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+
+            SimpleCalculator sc = new SimpleCalculator();
+            Console.WriteLine("Вводите слагаемые через запятую!\n" +
+                              "Используйте только целые числа!\n" +
+                              "Для выхода из программы нажмите Esq");
+            while (true)
+            {
+                try
+                {
+                    cki = Console.ReadKey(true);
+                    if (cki.Key == ConsoleKey.Escape) break;
+                    int sum = sc.AddOfSomeNumbers(Console.ReadLine());
+                    Console.WriteLine(sum);
+
+                }
+
+
+                catch
+                {
+                    Console.WriteLine("Используйте только целые числа! \n" +
+                                      "Обязательно вводите их через запятую!");
+                }
+            }
+        }
+
+
+    }
+}
